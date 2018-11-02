3a5f2139c396dd342f3373ab01991fbfd46c982e
Irene Bogues <irenebogues@gmail.com>
2018-11-02 18:28:31 -0400

    initializing files

---
+++ javascript/apps.js
@@ -0,0 +1,15 @@
+// MTA API KEY: 3d3c75a8e083ee09c81331d07c069494
+
+//pseudocode:
+/* When adding trains, administrators should be able to submit the following:
+Train Name
+Destination
+First Train Time -- in military time
+Frequency -- in minutes
+Code this app to calculate when the next train will arrive; this should be relative to the current time.
+Users from many different machines must be able to view same train times. */
+
+
+
+
+
